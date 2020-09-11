ded980e07d38e864ea5182bc9a684b2be251af03
Aryan-dev007 <aryansinha6363@gmail.com>
2020-09-11 22:53:14 +0530

    main

--- week1/pair_wise_max/src/main.c
+++ week1/pair_wise_max/src/main.c
@@ -1,69 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define MAX(a,b) (((a)>(b))? a:b)
 
-long long int maxpairwiseresult(int a[], int size);
-void quicksort(int number[],int first,int last);
+// Don't know why it shows "UNKNOWN SIGNAL 11"
+long long int MaxPairwiseProduct(long long int numbers[], int size) {
+    long long int max_product = 0;
+    int n = size;
 
+    for (int first = 0; first < n; ++first) {
+        for (int second = first + 1; second < n; ++second) {
+        max_product = MAX(max_product, numbers[first] * numbers[second]);
+        }
+    }
 
+    return max_product;
+}
 
 int main() {
     int n;
     scanf("%d", &n);
-    int *arr;
-    arr = malloc(n * sizeof(int));
+    long long int *arr;
+    arr = calloc(n, sizeof(int));
 
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%lli", &arr[i]);
     }
 
-    quicksort(arr, 0, n-1);
 
+    printf("%lli", MaxPairwiseProduct(arr, n));
 
-    long long int result = maxpairwiseresult(arr, n);
-
-    printf("%lli\n", result);
 }
 
-long long int maxpairwiseresult(int a[], int size) {
-    long long int res = 0;
 
-    // for (int i = 0; i < size; ++i) {
-    //     for (int j = i+1; j < size; ++j) {
-    //         if (((long long)a[i]) * a[j] > res) {
-    //             res = ((long long)a[i]) * a[j];
-    //         }
-    //     }
-    // }
-    res = (long long)a[size - 1] * (long long) a[size - 2];
-
-    return res;
-}
-
-void quicksort(int number[],int first,int last) {
-   int i, j, pivot, temp;
-
-   if(first<last){
-      pivot=first;
-      i=first;
-      j=last;
-
-      while(i<j){
-         while(number[i]<=number[pivot]&&i<last)
-            i++;
-         while(number[j]>number[pivot])
-            j--;
-         if(i<j){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
-         }
-      }
-
-      temp=number[pivot];
-      number[pivot]=number[j];
-      number[j]=temp;
-      quicksort(number,first,j-1);
-      quicksort(number,j+1,last);
-
-   }
-}
